ab5dcb0cc7e16b61c58253c4909e8dac78d0c743
dwane richards <dwanerich@gmail.com>
2020-01-31 17:33:12 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,8 +13,7 @@ class Triangle
         if (s1 * s2 * s3) == 0 || (s1 + s2) <= s3 || (s1 + s3) <= s2 || (s2 + s3) <= s1
           begin
           raise TriangleError
-          # rescue TriangleError => error
-          # return error.message
+          
           end
 
       elsif s1 == s2 && s1 == s3
